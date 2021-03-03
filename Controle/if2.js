1c9ac97688fefa658e45666ce7f7d09c961bd57a
Alan Oliveira de Sousa <72306242+Aln1002@users.noreply.github.com>
2021-03-03 13:54:40 -0300

    Add files via upload

---
+++ Controle/if2.js
@@ -0,0 +1,12 @@
+function teste1(num) {
+    if (num > 7) {
+        console.log(num)
+        console.log('Final')
+    }
+}
+
+teste1(6)
+teste1(8)
+
+//aula destinada a mostrar que o if quando tem mais de 1 sentenca de codigo precisa de estar dentro da chaves para todas serem executadas.
+// nao use ";" nas estruturas de controle.
