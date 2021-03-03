710744f240c095ca222b2102616eb8f49b9ee55c
Alan Oliveira de Sousa <72306242+Aln1002@users.noreply.github.com>
2021-03-03 13:34:03 -0300

    Rename usando_var_em_loop.js to fundamentos_js/usando_var_em_loop.js

--- usando_var_em_loop.js
+++
@@ -1,5 +0,0 @@
-for (var i = 0; i < 10; i++) {
-    console.log(i)
-}
-console.log('i =', i)
-// o i so sai do laco, apartir do numero que ele for maior, quando na expressao, é falado que i < 10, logo limita i menor q 10, i nao é 9, i é 10.
