ea8e9178e0d7ac015890e78e0f870b9970841fed
Alan Oliveira de Sousa <72306242+Aln1002@users.noreply.github.com>
2021-03-08 09:14:49 -0300

    Create OO.js

---
+++ Objetos/OO.js
@@ -0,0 +1,22 @@
+// codigo não executavel
+
+// procedural 
+
+processamento (valor1, valor2, valor3) 
+
+// oo 
+
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        // ...
+    }
+}
+
+objeto.processamento() // o foco agora é o objeto
+
+// principios importantes:
+//1. abstração
+//2. encapsulamento
