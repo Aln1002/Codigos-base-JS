0371f75fc303ab30e002affdc21b3b845e95a6fa
Alan Oliveira <72306242+Aln1002@users.noreply.github.com>
2021-03-31 09:51:54 -0300

    Add files via upload

---
+++ ESnext/revisao3.js
@@ -0,0 +1,25 @@
+//ES8 object.values/object.entries
+const obj = {a: 1, b: 2, c: 3}
+console.log(Object.values(obj))
+console.log(Object.entries(obj))
+
+//melhorias na notacao literal
+const nome = 'carla'
+const pessoa = {
+    nome,
+    ola() {
+        return 'oi gente'
+    }
+}
+
+console.log(pessoa.nome, pessoa.ola())
+
+//class
+class Animal {}
+class Cachorro extends Animal {
+    falar() {
+        return 'auau'
+    }
+}
+
+console.log(new Cachorro().falar())
